6cdf14e717a509ee9350a843e9d50db69a058b21
Ekaterina Kulpina <125750430+katrinakulpina@users.noreply.github.com>
2023-04-13 14:53:56 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
-from hyppo.ksample import MMD
+from scipy.stats import anderson_ksamp
 
 chat_id = 324047628 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     alpha = 0.1
-    _, pval = MMD(compute_kernel="laplacian", gamma=10).test(x, y)
+    pval = anderson_ksamp([x, y]).pvalue
     return pval < alpha
